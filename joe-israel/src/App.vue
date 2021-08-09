abdd48dd21d5a89459f1dbd115c88d6a80dd969d
Unobraid <77030734+Unobraid@users.noreply.github.com>
2021-08-09 05:16:50 -0300

    Empty Project
    Initial Vue project.

---
+++ joe-israel/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Joe Israel</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<style>
+body {
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
